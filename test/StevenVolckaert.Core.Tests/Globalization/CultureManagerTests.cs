86be4160739a350641e535f1805eb046a75331bd
Steven Volckaert <steven.volckaert@gmail.com>
2016-12-13 20:30:21 +0100

    Adding test project StevenVolckaert.Core.Tests.

---
+++ test/StevenVolckaert.Core.Tests/Globalization/CultureManagerTests.cs
@@ -0,0 +1,90 @@
+﻿namespace StevenVolckaert.Globalization.Tests
+{
+    using Globalization;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CultureManagerTests
+    {
+        /* TODO Implement the unit tests. For instance: Calling any of the SetCulture methods should return the name
+         * of the culture after execution, which must equal the CurrentCultureName property of the culture manager
+         * (after execution). Steven Volckaert. December 13, 2016.
+         */
+
+        //[TestMethod]
+        public void CultureManagerTest()
+        {
+            Assert.Fail();
+        }
+
+        //[TestMethod]
+        public void CultureManagerTest1()
+        {
+            Assert.Fail();
+        }
+
+        //[TestMethod]
+        public void GetCultureInfoTest()
+        {
+            Assert.Fail();
+        }
+
+        //[TestMethod]
+        public void GetNeutralCultureNameTest()
+        {
+            Assert.Fail();
+        }
+
+        //[TestMethod]
+        public void IsCultureSupportedTest()
+        {
+            Assert.Fail();
+        }
+
+        //[TestMethod]
+        public void IsSpecificCultureSupportedTest()
+        {
+            Assert.Fail();
+        }
+
+        //[TestMethod]
+        public void IsCultureSelectedTest()
+        {
+            Assert.Fail();
+        }
+
+        //[TestMethod]
+        public void SetCultureTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SetCultureTest2()
+        {
+            var cultureManager =
+                new CultureManager(
+                    defaultCultureName: "en-US",
+                    supportedCultureNames: new string[] { "en-US", "nl-BE" }
+                );
+
+            Assert.AreEqual("en-US", cultureManager.SetCulture("pt", "zh-Hans"));
+            Assert.AreEqual("nl-BE", cultureManager.SetCulture("pt", "zh-Hans", "nl-BE"));
+
+            // TODO Add additional asserts.
+            // Steven Volckaert. December 13, 2016.
+        }
+
+        //[TestMethod]
+        public void SetDefaultCultureTest()
+        {
+            Assert.Fail();
+        }
+
+        //[TestMethod]
+        public void SetSpecificCultureTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ test/StevenVolckaert.Core.Tests/UnitConvertTests.cs
@@ -0,0 +1,81 @@
+﻿namespace StevenVolckaert.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitConvertTests
+    {
+        /// <summary>
+        /// A list of tuples that links degrees Celsius to degrees Fahrenheit.
+        /// Source http://www.rapidtables.com/convert/temperature/celsius-to-fahrenheit.htm
+        /// </summary>
+        private static readonly List<Tuple<int, int>> _temperatureTuples =
+            new List<Tuple<int, int>>
+            {
+                new Tuple<int, int>(-50, -58),
+                new Tuple<int, int>(-40, -40),
+                new Tuple<int, int>(-30, -22),
+                new Tuple<int, int>(-20, -4),
+                new Tuple<int, int>(-10, 14),
+                new Tuple<int, int>(-9, 16),
+                new Tuple<int, int>(-8, 18),
+                new Tuple<int, int>(-7, 19),
+                new Tuple<int, int>(-6, 21),
+                new Tuple<int, int>(-5, 23),
+                new Tuple<int, int>(-4, 25),
+                new Tuple<int, int>(-3, 27),
+                new Tuple<int, int>(-2, 28),
+                new Tuple<int, int>(-1, 30),
+                new Tuple<int, int>(0, 32),
+                new Tuple<int, int>(1, 34),
+                new Tuple<int, int>(2, 36),
+                new Tuple<int, int>(3, 37),
+                new Tuple<int, int>(4, 39),
+                new Tuple<int, int>(5, 41),
+                new Tuple<int, int>(6, 43),
+                new Tuple<int, int>(7, 45),
+                new Tuple<int, int>(8, 46),
+                new Tuple<int, int>(9, 48),
+                new Tuple<int, int>(10, 50),
+                new Tuple<int, int>(20, 68),
+                new Tuple<int, int>(21, 70),
+                new Tuple<int, int>(30, 86),
+                new Tuple<int, int>(37, 99),
+                new Tuple<int, int>(40, 104),
+                new Tuple<int, int>(50, 122),
+                new Tuple<int, int>(60, 140),
+                new Tuple<int, int>(70, 158),
+                new Tuple<int, int>(80, 176),
+                new Tuple<int, int>(90, 194),
+                new Tuple<int, int>(100, 212),
+                new Tuple<int, int>(200, 392),
+                new Tuple<int, int>(300, 572),
+            };
+
+        [TestMethod]
+        public void FromCelsiusToFahrenheitTest()
+        {
+            _temperatureTuples.ForEach(x => Assert.AreEqual(x.Item2, UnitConvert.FromCelsiusToFahrenheit(x.Item1)));
+        }
+
+        [TestMethod]
+        public void FromFahrenheitToCelsiusTest()
+        {
+            _temperatureTuples.ForEach(x => Assert.AreEqual(x.Item1, UnitConvert.FromFahrenheitToCelsius(x.Item2)));
+        }
+
+        //[TestMethod]
+        public void FromFeetToMeterTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        //[TestMethod]
+        public void FromMeterToFeetTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
