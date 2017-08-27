7c78ef1f6a70ec299aacce80c85b5791bf12a8cb
Steven Volckaert <steven.volckaert@gmail.com>
2017-08-27 12:37:03 +0200

    Adding unit test CanAccessResources.

---
+++ test/StevenVolckaert.Core.Tests/ResourcesTests.cs
@@ -0,0 +1,17 @@
+﻿namespace StevenVolckaert.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ResourcesTests
+    {
+        [TestMethod]
+        public void CanAccessResources()
+        {
+            var resourceManager = Resources.ResourceManager;
+
+            Assert.IsNotNull(resourceManager);
+            Assert.IsNotNull(Resources.ValueNullEmptyOrWhiteSpace);
+        }
+    }
+}
